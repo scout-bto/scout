2c24af45b48c5d36d86ad81c851183bb71313900
Yingli <126117333+Yingli-NLR@users.noreply.github.com>
2026-06-23 09:31:28 -0600

    AEO Update (#613)
    * aeo update
    
    * cambium update
    
    * cambium update
    
    * source descriptions for emm_region_emissions files
    
    * Upload plot comparison images from CI build
    
    * baseline comparision code update
    
    * code quality
    
    * check-PR error
    
    * 2026 data update
    
    * 2026 data update
    
    * 2026 data update
    
    * AEO update code improvement
    
    * data update
    
    * data update
    
    * baseline comparision update
    
    * Upload plot comparison images from CI build
    
    * office equip
    
    * office equip
    
    * broden SDOUT
    
    * broaden SDOUT
    
    * Upload plot comparison images from CI build
    
    * plug load enduse
    
    * Upload plot comparison images from CI build
    
    * cpl file for data center
    
    * cpl file for data center
    
    * Upload plot comparison images from CI build
    
    * remove data center enduse for plug load measure
    
    * Upload plot comparison images from CI build
    
    * cost name
    
    ---------
    
    Co-authored-by: Yingli <ylou2@nrel.gov>
    Co-authored-by: GitHub Action <github-action@users.noreply.github.com>

--- scout/cambium_updater.py
+++ scout/cambium_updater.py
@@ -65,7 +65,10 @@ import json
 import gzip
 import re
 from pathlib import Path
+from dotenv import load_dotenv
 from scout.config import FilePaths as fp
+
+load_dotenv()
 
 
 class UsefulInputFiles(object):
